5c36fc6052e269e4e31d4291e5bd15448815e1b4
Vasili <albergvasili@yandex.com>
2023-03-10 19:17:06 -0500

    Update paragraph texts

--- src/App.js
+++ src/App.js
@@ -29,30 +29,62 @@ function App() {
     Pas besoin d'ajouter de l'eau à l'océan plein d'hydroxyde d'hydrogène.   
         `;
   const thirdParagraphe = `
-        Il n'y a là rien de spécial. Mais je ne cherche pas à être spécial, congosà! Je veux tout
-        simplement éviter de tomber dans la cataracte! Catastrophe! Catastérisme! Pardon. Je
-        reprends: L'eau qui se jette dans l'eau. Inutile redondance de cyberespaces (utiles et
-        simples). Ce n'est pas pour gagner de l'argent que je songe à créer ce site, cela est clair.
-        Sinon je dirais bien: que l'eau appelle l'eau, et que l'argent appelle l'argent.
+    Il n'y a là rien de spécial. 
+
+    Mais je ne cherche pas à être spécial,  congosà!   
+
+    Je veux tout simplement éviter de tomber dans la cataracte! 
+    Catastrophe! 
+    Catastérisme! 
+    Pardon.   
+
+    Je reprends: 
+    L'eau qui se jette dans l'eau.   
+
+    Inutile redondance de cyberespaces (utiles et simples).   
+
+    Ce n'est pas pour gagner de l'argent que je songe à créer un site web, 
+    cela est clair.   
+
+    Sinon je dirais bien: 
+    que l'eau appelle l'eau, 
+    et que l'argent appelle l'argent.   
         `;
   const fourthParagraphe = `
-        J'ai plongé aujourd'hui dans les eaux de la virtualité pour créer quelque chose digne d'être
-        apprécié. Je veux que mon site web soit *estiminda*
-         . L'utilité ne figure donc pas dans
-        l'horizon d'attente. La simplicité? Peut-être, parfois, pourquoi pas? Le but? Il me semble
-        que concevoir son but est possible, mais l'essence de ce but est un mystère dans les
-        profondeurs des eaux qui nous submergent.
+    J'ai plongé aujourd'hui dans les eaux de la virtualité pour créer quelque chose digne d'être apprécié.   
+
+    Je veux que mon site web soit *estiminda*.   
+
+    L'utilité ne figure donc pas dans l'horizon d'attente.   
+
+    Et la simplicité?   
+
+    Peut-être, 
+    parfois, 
+    pourquoi pas?   
+
+    Le but?   
+
+    Il me semble que concevoir un but est possible, 
+    mais l'essence de ce but est un mystère dans les profondeurs des eaux qui nous submergent.   
         `;
   const fifthParagraphe = `
-        Remarquons que j'ai certainement quelque chose à dire, puisque je dis des choses, que je
-        n'ai absolument pas le désir de promouvoir un service pour quelque profit, et que la route
-        que je tente d'établir dans les eaux trompeuses de mon imagination n'a ni de destination
-        précise dans ses hyperliens, ni l'intention de ne pas chercher à s'égarer. C'est à dire, que
-        nous nageons ici dans des eaux étranges, et le site que voici est véritablement à rebours.
+    Remarquons que j'ai certainement quelque chose à dire, 
+    puisque je dis des choses, 
+    que je n'ai absolument pas le désir de promouvoir un service pour quelque profit, 
+    et que la route que je tente d'établir dans les eaux trompeuses de mon imagination n'a 
+    ni de destination précise dans ses hyperliens, 
+    ni l'intention de ne pas chercher à s'égarer.   
+
+    C'est à dire, 
+    que nous nageons ici dans des eaux étranges, 
+    et le site que voici est véritablement à rebours.   
         `;
   const sixthParagraphe =  `
-        Le séjour en cet espace cybérien surréaliste est inutile, incomplet, et bizarre.
-        Bienvenue au labyrinthe des folies du Cybernaute!
+    Le séjour en cet espace cybérien surréaliste est inutile, 
+    incomplet, 
+    et bizarre.   
+    Bienvenue au labyrinthe des folies du Cybernaute!
         `;
   function sleep(time){
     return new Promise(r => setTimeout (r, time));
