88a6fe4d23ecbb9d3b16141f925bbf24cbe90012
Vasili <albergvasili@yandex.com>
2023-03-18 14:12:49 -0400

    Switch rendered component
    -Define State to determine component to be rendered
    -Use switch statement to change component rendered
    -Move body tag styles to src/index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
 import Bienvenue from './components/Bienvenue';
+import Main from './components/Main';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Bienvenue />
-    </div>
-  );
+  const [rendering, setRendering] = useState("main");
+
+  const enter = () => {
+    setRendering("intro");
+  };
+
+  switch (rendering) {
+    case 'main':
+      return (<Main onClick={enter} />);
+    case 'intro':
+      return (<Bienvenue />);
+    default:
+  }
 };
 
 export default App;
