a14225ebc3700281eec1e60dc31a651039ea176b
Vasili <albergvasili@yandex.com>
2023-03-18 13:02:13 -0400

    Move backend to server directory
    -Create new node project (add server dependencies)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+// const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('./build'));
+
+/*
+app.use('/test', express.static(path.join(__dirname+'/status_codes')));
+app.get('/test', (req, res) => {
+  res.sendFile(path.join(__dirname+'/status_codes/410.html'));
+});
+*/
 
 const PORT = 3001;
 app.listen(PORT, () => {
