14b8d9e26716206946a3da54a7e2c403eaf46b2d
Vasili <albergvasili@yandex.com>
2023-04-13 21:28:25 -0400

    Create Footer component
    -Add footer to root container

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+
+function Footer({ props }) {
+  return (
+    <footer>
+      <form>
+        <button id="main-menu" type="submit">Retour au Menu Principal</button>
+      </form>
+    </footer>
+    
+  );
+};
+
+export default Footer;
