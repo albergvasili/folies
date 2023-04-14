3a6a348610383a6ca17f4f03c0a62e3c9b761c63
Vasili <albergvasili@yandex.com>
2023-04-13 21:24:14 -0400

    Allow chapter selection from Main Menu
    -Add buttons in Main.js to switch to each component
    -Update App.js to render Escape, Water, and Tarot components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import Bienvenue from './components/Bienvenue';
 import Main from './components/Main';
 import Who from './components/Who';
+import Escape from './components/Escape';
+import Water from './components/Water';
+import Tarot from './components/Tarot';
 
 import { useState } from 'react';
 
@@ -9,12 +12,17 @@ function App() {
 
   switch (rendering) {
     case 'main':
-      return (<Main onClick={ () => setRendering('intro') } />);
+      return (<Main onClick={ (renderChoice) => setRendering(renderChoice) } />);
     case 'intro':
       return (<Bienvenue onClick={ () => setRendering('who') }/>);
     case 'who':
       return (<Who onClick={ () => console.log('ça marche') }/>);
-
+    case 'escape':
+      return (<Escape />);
+    case 'water':
+      return (<Water />);
+    case 'tarot':
+      return (<Tarot />);
     default:
   }
 };
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,14 @@
-function Main({onClick}) {
+function Main({ onClick }) {
   return (
     <div>
       <h1>Le Cybernaute</h1>
         <h2>{"Nul n'entre ici s'il n'est philosophe"}</h2>
-        <div>
-          <input type="button" onClick={onClick} value="Entrer"/>
+        <div className="options">
+          <button type="button" onClick={ () => onClick("intro") }>Intro</button>
+          <button type="button" onClick={ () => onClick("who") }>Qui suis-je?</button>
+          <button type="button" onClick={ () => onClick("escape") }>Escape</button>
+          <button type="button" onClick={ () => onClick("water") }>Water</button>
+          <button type="button" onClick={ () => onClick("tarot") }>Tarot</button>
         </div>
     </div>
   );
